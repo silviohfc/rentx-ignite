8a33e3fc7a46c60a8920bc7fa41a448391052d8f
Silvio Henrique <silviohfc@gmail.com>
2021-03-27 19:23:30 -0300

    add api

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,28 @@
+import { Router } from "express";
+import multer from "multer";
+
+import { CreateCategoryController } from "../modules/cars/usecases/createCategory/CreateCategoryController";
+import { ImportCategoryController } from "../modules/cars/usecases/importCategory/ImportCategoryController";
+import { ListCategoriesController } from "../modules/cars/usecases/listCategories/ListCategoriesController";
+
+const categoriesRoutes = Router();
+
+const upload = multer({
+  dest: "./tmp",
+});
+
+const createCategoryController = new CreateCategoryController();
+const importCategoryController = new ImportCategoryController();
+const listCategoriesController = new ListCategoriesController();
+
+categoriesRoutes.post("/", createCategoryController.handle);
+
+categoriesRoutes.get("/", listCategoriesController.handle);
+
+categoriesRoutes.post(
+  "/import",
+  upload.single("file"),
+  importCategoryController.handle
+);
+
+export { categoriesRoutes };
